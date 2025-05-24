24e226190dbdc26716c884de997b4d4ffc77921e
unional <homawong@gmail.com>
2025-05-24 02:32:51 -0700

    docs: fix manager.ts

--- libs/types/.storybook/manager.ts
+++ libs/types/.storybook/manager.ts
@@ -1,4 +1,4 @@
-import { tagBadges } from '@repobuddy/storybook/manager'
+import { tagBadges } from '@repobuddy/storybook/storybook-addon-tag-badges'
 import { addons } from '@storybook/manager-api'
 
 addons.setConfig({
