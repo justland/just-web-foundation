4785c8e0133208d381a1b565cf1646c82d5ca8b6
Homa Wong <hwong@paloaltonetworks.com>
2025-07-07 07:52:41 -0700

    chore: skip docs test

--- apps/docs/vitest.config.ts
+++ apps/docs/vitest.config.ts
@@ -1,5 +1,5 @@
 import { browserTestPreset } from '@repobuddy/vitest/config'
-import storybookTest from '@storybook/experimental-addon-test/vitest-plugin'
+import storybookTest from '@storybook/addon-vitest/vitest-plugin'
 import { join } from 'node:path'
 import { storybookVis } from 'storybook-addon-vis/vitest-plugin'
 import { defineConfig } from 'vitest/config'
