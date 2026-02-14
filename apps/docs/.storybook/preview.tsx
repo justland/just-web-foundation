f408d1a068c9b34f47ee25f8503d803a2a73543e
unional <homawong@gmail.com>
2026-02-14 00:23:37 -0800

    refactor: extract monaco editor configuration into separate file

--- apps/docs/.storybook/preview.tsx
+++ apps/docs/.storybook/preview.tsx
@@ -1,9 +1,13 @@
 import { defineParameters } from '@repobuddy/storybook'
 import { defineDarkModeParam } from '@repobuddy/storybook/storybook-dark-mode'
 import type { Preview } from '@storybook/react-vite'
+import { setupMonaco } from 'storybook-addon-code-editor'
+import { onMonacoLoad } from '../../../libs/toolkits/.storybook/code-editor.ts'
 
 import './tailwind.css'
 import './tailwind.repobuddy-storybook.css'
+
+setupMonaco({ onMonacoLoad })
 
 const preview: Preview = {
 	parameters: defineParameters(
