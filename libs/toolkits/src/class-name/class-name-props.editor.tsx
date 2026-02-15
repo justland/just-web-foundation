49043aef6431b4faadfee4f971ed08cf44ccd69b
unional <homawong@gmail.com>
2026-02-14 22:49:34 -0800

    refactor(toolkits): update class-name-props stories and editor source

--- libs/toolkits/src/class-name/class-name-props.editor.tsx
+++ libs/toolkits/src/class-name/class-name-props.editor.tsx
@@ -1,8 +1,13 @@
 import type { ClassNameProps } from '@just-web/toolkits'
+import { StoryCard } from '@repobuddy/storybook'
 import type { PropsWithChildren } from 'react'
+
+export default () => (
+	<StoryCard appearance="output">
+		<MyComponent className="font-extrabold">Hello in extra bold</MyComponent>
+	</StoryCard>
+)
 
 function MyComponent({ className, children }: PropsWithChildren<ClassNameProps>) {
 	return <div className={className}>{children}</div>
 }
-
-export default () => <MyComponent className="text-blue-800">Hello in blue</MyComponent>
