9e5bffbffe05e51e51dcbc04d41b5160db64e043
unional <homawong@gmail.com>
2026-02-27 21:22:29 -0800

    refactor(toolkits): update class-name-props stories and editor source

--- libs/toolkits/src/class-name/class-name-props.stories.tsx
+++ libs/toolkits/src/class-name/class-name-props.stories.tsx
@@ -1,34 +1,53 @@
+import * as repobuddyStorybook from '@repobuddy/storybook'
 import { defineDocsParam, showDocSource, withStoryCard } from '@repobuddy/storybook'
 import type { Meta, StoryObj } from '@repobuddy/storybook/storybook-addon-tag-badges'
 import * as React from 'react'
 import { makeLiveEditStory } from 'storybook-addon-code-editor'
 import * as toolkits from '#just-web/toolkits'
-import code from './class-name-props.editor.src.tsx?raw'
+import code from './class-name-props.editor.tsx?raw'
 import source from './class-name-props.ts?raw'
 
 const meta: Meta<toolkits.ClassNameProps> = {
 	title: 'class-name/ClassNameProps',
-	tags: ['type', 'version:next'],
+	tags: ['type', 'version:next', '!test'],
 	render: () => <></>,
 }
 
 export default meta
 
-export const Playground: StoryObj = {
-	tags: ['!test', 'editor'],
-	parameters: defineDocsParam({ source: { code } }),
+export const Specification: StoryObj = {
+	tags: ['source'],
+	parameters: defineDocsParam({
+		source: { code: source },
+	}),
+	decorators: [
+		withStoryCard({
+			content: (
+				<p>
+					<code>ClassNameProps</code> describes the standard type of the <code>className</code> property.
+				</p>
+			),
+		}),
+		showDocSource(),
+	],
 }
 
-makeLiveEditStory(Playground, {
+export const BasicUsage: StoryObj = {
+	tags: ['use-case', 'editor'],
+	parameters: defineDocsParam({ source: { code } }),
+	decorators: [
+		withStoryCard({
+			content: <p>Use it by itself or combine with other props.</p>,
+		}),
+		showDocSource({ placement: 'before' }),
+	],
+}
+
+makeLiveEditStory(BasicUsage, {
 	availableImports: {
 		'@just-web/toolkits': toolkits,
+		'@repobuddy/storybook': repobuddyStorybook,
 		react: React,
 	},
-	code: Playground.parameters?.['docs']?.['source']?.code,
+	code: BasicUsage.parameters?.['docs']?.['source']?.code,
 })
-
-export const Source: StoryObj = {
-	tags: ['!test', 'source'],
-	parameters: defineDocsParam({ source: { code: source } }),
-	decorators: [withStoryCard(), showDocSource({ placement: 'before' })],
-}
