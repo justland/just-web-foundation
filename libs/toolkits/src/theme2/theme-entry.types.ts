c1afc2e7d37a9b3e58c74e5f7d1e82cbd4cc2bf5
unional <homawong@gmail.com>
2026-02-28 15:18:24 -0800

    docs(theme2): add JSDoc for ThemeEntry type

--- libs/toolkits/src/theme2/theme-entry.types.ts
+++ libs/toolkits/src/theme2/theme-entry.types.ts
@@ -1,5 +1,10 @@
 import type { ThemeMap } from './theme.types.ts'
 
+/**
+ * Theme entry is a pair of theme key and theme value.
+ *
+ * It is the basic value persisted by the theme stores.
+ */
 export type ThemeEntry<Themes extends ThemeMap = ThemeMap> = {
 	theme: keyof Themes
 	value: Themes[keyof Themes]
