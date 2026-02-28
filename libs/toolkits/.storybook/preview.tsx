8c7865581678c5981d1d04a689eb280f66b1e0b9
unional <homawong@gmail.com>
2026-02-27 21:22:02 -0800

    refactor: extract monaco editor configuration into separate file

--- libs/toolkits/.storybook/preview.tsx
+++ libs/toolkits/.storybook/preview.tsx
@@ -2,33 +2,12 @@ import { defineParameters } from '@repobuddy/storybook'
 import { withThemeByClassName } from '@storybook/addon-themes'
 import type { Preview } from '@storybook/react-vite'
 import { setupMonaco } from 'storybook-addon-code-editor'
-import ToolkitTypes from '../.editor/just_web_toolkits/index.d.mts?raw'
-import RepobuddyStorybookTypes from '../.editor/repobuddy_storybook/index.d.mts?raw'
-import TypePlusTypes from '../.editor/type_plus/index.d.mts?raw'
 
 import './tailwind.css'
 import './tailwind.repobuddy-storybook.css'
+import { onMonacoLoad } from './code-editor.ts'
 
-setupMonaco({
-	onMonacoLoad(monaco) {
-		monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
-			noSemanticValidation: false,
-			noSyntaxValidation: false,
-		})
-		monaco.languages.typescript.typescriptDefaults.addExtraLib(
-			ToolkitTypes,
-			'file:///node_modules/@just-web/toolkits/index.d.ts',
-		)
-		monaco.languages.typescript.typescriptDefaults.addExtraLib(
-			RepobuddyStorybookTypes,
-			'file:///node_modules/@repobuddy/storybook/index.d.ts',
-		)
-		monaco.languages.typescript.typescriptDefaults.addExtraLib(
-			TypePlusTypes,
-			'file:///node_modules/type-plus/index.d.ts',
-		)
-	},
-})
+setupMonaco({ onMonacoLoad })
 
 const preview: Preview = {
 	parameters: defineParameters(
