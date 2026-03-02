2ff599a62cc1f40305aac733539f1ddc5dd9c04a
unional <homawong@gmail.com>
2026-03-01 23:07:36 -0800

    docs(storybook): switch manager theme to dark and add brandUrl
    - Replace `themes.normal` spread with `create({ base: 'dark' })` in manager.ts
    - Add `brandUrl` pointing to GitHub repository for both docs and toolkits

--- apps/docs/.storybook/manager.ts
+++ apps/docs/.storybook/manager.ts
@@ -1,11 +1,12 @@
 import { tagBadges } from '@repobuddy/storybook/storybook-addon-tag-badges'
 import { addons } from 'storybook/manager-api'
-import { themes } from 'storybook/theming'
+import { create } from 'storybook/theming'
 
 addons.setConfig({
 	tagBadges,
-	theme: {
-		...themes.normal,
-		brandTitle: '@just-web/fundation'
-	}
+	theme: create({
+		base: 'dark',
+		brandTitle: '@just-web/fundation',
+		brandUrl: 'https://github.com/justland/just-web-foundation'
+	})
 })
--- libs/toolkits/.storybook/manager.ts
+++ libs/toolkits/.storybook/manager.ts
@@ -1,11 +1,12 @@
 import { tagBadges } from '@repobuddy/storybook/storybook-addon-tag-badges'
 import { addons } from 'storybook/manager-api'
-import { themes } from 'storybook/theming'
+import { create } from 'storybook/theming'
 
 addons.setConfig({
 	tagBadges,
-	theme: {
-		...themes.normal,
-		brandTitle: '@just-web/toolkits'
-	}
+	theme: create({
+		base: 'dark',
+		brandTitle: '@just-web/toolkits',
+		brandUrl: 'https://github.com/justland/just-web-foundation/tree/main/libs/toolkits'
+	})
 })
